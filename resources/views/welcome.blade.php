6da17eee4ad7da3bcdb0e3556ce3d2caffafb441
Zdakman <a_sadiki@esi.dz>
2017-01-04 15:20:27 +0100

    second changes
    hay

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel</div>
+                <div class="title">Laravel ayoub</div>
             </div>
         </div>
     </body>
